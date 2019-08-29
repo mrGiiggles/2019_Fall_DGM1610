870626c6f182137df3aef2a0625ae46c16006b37
bryce <banderson7370@gmail.com>
2019-08-29 15:39:29 -0600

    changed unity version & hello world

--- Project One/Assets/Scripts/StartScript.cs
+++ Project One/Assets/Scripts/StartScript.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Practice : MonoBehaviour
+public class StartScript : MonoBehaviour
 {
-    // Use this for initialization
-    void Start()
+    // Start is called before the first frame update
+    private void Start()
     {
+        print("Hello World");
     }
 
     // Update is called once per frame
