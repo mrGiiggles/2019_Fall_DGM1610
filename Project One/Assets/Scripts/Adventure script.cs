97be18880f923bcc9030cffdcd6cca7d4ebbed4a
bryce <banderson7370@gmail.com>
2019-09-03 00:00:50 -0600

    begining of adventure switch statements

--- Project One/Assets/Scripts/Adventure script.cs
+++ Project One/Assets/Scripts/Adventure script.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Adventurescript : MonoBehaviour
 {
+    public enum States
+    {
+        tavern,
+        bar,
+        table,
+        order,
+        food,
+        drink
+    };
+
+    public States currentstate;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentstate = States.tavern;
     }
 
     // Update is called once per frame
     void Update()
     {
+        switch (currentstate)
+        { case States.tavern:
+            tavern();
+            break;
+            
+        }
         
     }
 }
