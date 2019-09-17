09e0ae410afca07a3108198fa68e9dab31125439
bryce <banderson7370@gmail.com>
2019-09-17 15:46:25 -0600

    character movement working good

---
+++ Project One/Assets/Scripts/Movement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+public class Movement : MonoBehaviour
+{
+    private Vector3 position;
+    public CharacterController controller;
+    
+    public float speed = 1f;
+    public float gravity = -3f;
+    public float jumpSpeed = 100f;
+    void Update()
+    {
+        position.x = speed * Input.GetAxis("Horizontal");
+
+        if (!controller.isGrounded)
+        {
+            position.y -= gravity;
+        }
+        else
+        {
+            position.y = 0f;
+        }
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            position.y = jumpSpeed;
+        }
+
+        controller.Move(position*Time.deltaTime);
+    }
+}
