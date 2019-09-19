1836b6a3b8896ee50dafd6bca89d9efd14bbabcb
bryce <banderson7370@gmail.com>
2019-09-19 15:46:15 -0600

    made new movement script and did double jumpe

--- Project One/Assets/Scripts/Movement.cs
+++ Project One/Assets/Scripts/Movement.cs
@@ -1,33 +1,43 @@
-﻿using UnityEngine;
-using UnityEngine.XR.WSA.Input;
+﻿
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
 
 public class Movement : MonoBehaviour
 {
     private Vector3 position;
-    public CharacterController controller;
+    private CharacterController controller;
     
-    public float speed = 1f;
-    public float gravity = -3f;
-    public float jumpSpeed = 100f;
+    public float moveSpeed = 10f, gravity = 9.81f, jumpSpeed = 30f;
+    private int jumpCount;
+    public int jumpCountMax = 2;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        position.x = speed * Input.GetAxis("Horizontal");
+        position.x = moveSpeed*Input.GetAxis("Horizontal");
+        position.y -= gravity;
 
-        if (!controller.isGrounded)
+        if (controller.isGrounded)
         {
-            position.y -= gravity;
-        }
-        else
-        {
-            position.y = 0f;
+            position.y = 0;
+            jumpCount = 0;
         }
 
-
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && jumpCount < jumpCountMax)
         {
             position.y = jumpSpeed;
+            jumpCount++;
         }
-
         controller.Move(position*Time.deltaTime);
+        
+
     }
 }
