81d70aa449177a0e043e5fc9444b75f1b6683308
bryce <banderson7370@gmail.com>
2019-09-24 15:52:53 -0600

    iff statements and rigid bodys

---
+++ Project One/Assets/Scripts/GetFroot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetFroot : MonoBehaviour
+{
+    public bool appleIsRipe, orangeIsRipe, peachIsRipe;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (appleIsRipe)
+        {
+            print("Go Pick the Apple");
+        }
+        else if (orangeIsRipe)
+        {
+            print("Go Pick the Orange");
+        } else if (peachIsRipe)
+        {
+            print("Go Pick the Peach");
+        }
+        else
+        {
+            print("Nothing is Ripe");
+        }
+        
+    }
+
+    
+}
---
+++ Project One/Assets/Scripts/IfStatements.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+
+    public int a = 4;
+    public int b = 5;
+    public int c = 10;
+    public string password = "Friend";
+    public bool lightsOn = false;
+    void Start()
+    {
+        if (a + b < c)
+        {
+            print(true);
+
+        }
+        else
+        {
+            print(false);
+        }
+        
+
+        if (password != "OU812")
+        {
+           print(true); 
+        }
+        
+
+        if (!lightsOn)
+        {
+            print(true);
+        }
+        
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
