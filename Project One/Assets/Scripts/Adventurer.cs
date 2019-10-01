b6b4cddc3c7107330956d6f00e847dc1f00f9df9
bryce <banderson7370@gmail.com>
2019-10-01 11:44:17 -0600

    made scriptable object

---
+++ Project One/Assets/Scripts/Adventurer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Adventurers", menuName = "Chosen ones")]
+public class Adventurer : ScriptableObject
+{
+    public string figthingClass;
+    public string name;
+
+    public int health;
+    public int mana;
+    public int stamina;
+
+    public int charisma;
+    public int finesse;
+
+    public GameObject weapon;
+}
