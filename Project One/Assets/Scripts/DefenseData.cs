b32c43987beca2bd753f27af02d441685d1778c4
bryce <banderson7370@gmail.com>
2019-10-09 23:56:17 -0600

    created DefenseData script

---
+++ Project One/Assets/Scripts/DefenseData.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class DefenseData : ScriptableObject
+{
+    public Sprite sprite;
+    public Color color;
+    public int defense;
+    public string rarity;
+}
