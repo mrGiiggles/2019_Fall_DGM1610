c7cab0bac949b17f0c8597dd4bdfd240e2299b08
bryce <banderson7370@gmail.com>
2019-10-15 15:48:24 -0600

    made camera to follow player

---
+++ Project One/Assets/Scripts/CinneMachineController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+[RequireComponent(typeof(CinemachineVirtualCamera))]
+public class CinneMachineController : MonoBehaviour
+{
+    public PlayerData playerData;
+    private CinemachineVirtualCamera virtualCamera;
+    void Awake()
+    {
+        virtualCamera = GetComponent<CinemachineVirtualCamera>();
+        playerData.instanceAction = InstanceHandler;
+        playerData.InstancePlayer();
+    }
+
+    private void InstanceHandler(GameObject obj)
+    {
+        virtualCamera.Follow = obj.transform;
+    }
+}
--- Project One/Assets/Scripts/DataScripts/PlayerData.cs
+++ Project One/Assets/Scripts/DataScripts/PlayerData.cs
@@ -1,10 +1,23 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
 [CreateAssetMenu] 
 public class PlayerData : GameArtData
 {
+    public UnityAction<GameObject> instanceAction;
+    
     public FloatData health;
     public List<WeaponData> weapons;
     public ClothesData shirt;
     public ClothesData pants;
+
+    public void InstancePlayer()
+    {
+        var newPlayer = Instantiate(prefab);
+        var playerSprite = newPlayer.GetComponentInChildren<SpriteRenderer>();
+        playerSprite.sprite = sprite;
+        playerSprite.color = spriteColor;
+        instanceAction(newPlayer);
+    }
 }
