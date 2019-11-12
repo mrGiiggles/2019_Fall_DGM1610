d1c8c21d24ad2d65cb11143a642f8fedbf322b91
bryce <banderson7370@gmail.com>
2019-11-12 15:49:26 -0700

    coroutines

---
+++ Project One/Assets/Scripts/Coroutines/Coroutines.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Xml.Serialization;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Coroutines : MonoBehaviour
+{
+    public UnityEvent startEvent, repeatEvent, endEvent;
+    public float seconds = 10;
+    public int counter = 10;
+    public bool canRun;
+    
+    private WaitForSeconds wfsObj;
+
+    private void Awake()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+    }
+
+    public void CallCoroutine()
+    {
+        StartCoroutine(RunCoroutine());
+    }
+    
+    
+    IEnumerator RunCoroutine()
+    {
+        startEvent.Invoke();
+        while (counter > 0)
+        {
+            repeatEvent.Invoke();
+            yield return wfsObj;
+            counter--;
+        }
+        
+        while (canRun)
+        {
+            repeatEvent.Invoke();
+            yield return wfsObj;
+            counter--;
+        }
+        
+        
+        endEvent.Invoke();
+    }
+
+}
