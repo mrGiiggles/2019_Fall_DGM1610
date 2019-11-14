c8c8582f43b4112e98f0cb1f3cff393e88373ce5
bryce <banderson7370@gmail.com>
2019-11-14 15:44:06 -0700

    Created Hlthup Coroutine

---
+++ Project One/Assets/Scripts/Coroutines/CouroutineClass.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.XR.Interaction;
+
+public class CouroutineClass : MonoBehaviour
+{
+    public bool canRun = true;
+    public IntData index;
+    public float seconds = 1f;
+    private WaitForSeconds wfsObj;
+
+    public void Run()
+    {
+        wfsObj = new WaitForSeconds(seconds);
+        StartCoroutine(OnRun());
+
+    }
+    //IEnumerator Start()
+    IEnumerator OnRun()
+    {
+        while (index.value > 0)
+        {
+            Debug.Log(index.value);
+            index.value--;
+            yield return wfsObj;
+            
+        }
+        
+        
+    }
+
+   
+}
---
+++ Project One/Assets/Scripts/Coroutines/HlthUpCoroutine.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HlthUpCoroutine : MonoBehaviour
+{
+    public bool bandAid = true;
+    public FloatData checkUp;
+
+    public void firstAid()
+    {
+        StartCoroutine(HlthUp());
+    }
+    IEnumerator HlthUp()
+    {
+        while (bandAid)
+        {
+            yield return new WaitForSeconds(1f);
+            checkUp.UpdateValue(1f);
+        }
+        
+    }
+
+    
+    
+}
