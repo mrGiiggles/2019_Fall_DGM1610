3d42bed1e002a62d0c051eea7889c51e70477204
bryce <banderson7370@gmail.com>
2019-11-21 15:39:26 -0700

    Animation and States Machine

---
+++ Project One/Assets/StatesBehavior.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class StatesBehavior : StateMachineBehaviour
+{
+    public UnityEvent StartEnterEvent, StateUpdateEvent, StateExitEvent;
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        StartEnterEvent.Invoke();
+    }
+
+    
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        StateUpdateEvent.Invoke();
+    }
+
+    
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+       StateExitEvent.Invoke(); 
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
